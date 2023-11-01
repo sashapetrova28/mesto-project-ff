2deb208b4c19f83f22e850a83ac20e56ede42f98
Александра Антоненко <sasha_281200@mail.ru>
2023-11-01 17:27:44 +0500

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,37 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template');
+const placesList = document.querySelector('.places__list');
+
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
 
+function addCard(item) {
+  const cardContent = cardTemplate.content.querySelector('.card').cloneNode(true);
+  const deleteButton = cardContent.querySelector('.card__delete-button');
+  
+  cardContent.querySelector('.card__title').textContent = item.name;
+  cardContent.querySelector('.card__image').src = item.link;
+  deleteButton.addEventListener('click', cardRemove);
+
+  return cardContent;
+}
+
 // @todo: Функция удаления карточки
 
+function cardRemove (evt) {
+  const card = evt.target.closest('.card');
+  card.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+function displayCardList() {
+  initialCards.forEach((item) => {
+    placesList.append(addCard(item));
+  });
+}
+
+displayCardList();
+
